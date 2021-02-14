e89b9a1aaaa89b7f2a4b41d40670c304ce02a51b
Folagbade Adeusi <zothuras@gmail.com>
2021-02-14 15:18:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,20 +8,20 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  num1*num2
-
-end
-
-def multiplication(num1, num2)
-  num1-num2
-
-end
-
-def modulo(num1, num2)
   num1/num2
 
 end
 
-def square_root(num)
+def multiplication(num1, num2)
+  num1*num2
+
+end
+
+def modulo(num1, num2)
   num1%num2
+
+end
+
+def square_root(num)
+  num1**(1/2)
 end
